fc6fa9fc094219f2f23f6b797413eaf0bd11fe87
Jim Kirkbride <james.kirkbride@gmail.com>
2016-06-28 21:53:25 -0400

    Failing Espresso test.

---
+++ app/src/androidTest/java/com/jameskbride/leastresistance/LeastResistanceActivityInstrumentationTest.java
@@ -0,0 +1,34 @@
+package com.jameskbride.leastresistance;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class LeastResistanceActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule activityTestRule = new ActivityTestRule<>(LeastResistanceActivity.class);
+
+    @Test
+    public void givenAMapWithAMinimalValidPathWhenThePathIsCalculatedThenItShouldReturnThePath() {
+        onView(withId(R.id.map_text)).perform(typeText("1 2 3 4 5"));
+
+        onView(withId(R.id.find_path_button)).perform(click());
+
+        onView(withText("Yes\n15\n1 1 1 1 1")).check(matches(isDisplayed()));
+    }
+}
