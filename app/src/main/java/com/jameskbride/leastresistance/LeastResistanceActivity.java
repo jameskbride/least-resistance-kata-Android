11f14361de340413c989d993ce2dba03540408c5
James Kirkbride <jim@JimKirkbrides-MacBook-Pro.local>
2016-06-28 21:16:51 -0400

    Initial commit.  Added the least-resistance lib.

---
+++ app/src/main/java/com/jameskbride/leastresistance/LeastResistanceActivity.java
@@ -0,0 +1,13 @@
+package com.jameskbride.leastresistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeastResistanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_least_resistance);
+    }
+}
